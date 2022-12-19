8ef991650ae930695a2a191bc4aa032184bce6f7
Hamza Magdy <76901476+HamzaMa96@users.noreply.github.com>
2022-12-19 17:30:32 +0200

    Add files via upload

--- initialize_DB_Tables.py
+++ initialize_DB_Tables.py
@@ -1,63 +1,93 @@
 import psycopg2
-
+import pyodbc
 # SQLite DB Name
-DB_Name =  "mqtt"
-hostname = 'localhost'
-username = 'postgres'
-pws = 'password@1324'
-port_id = 5432
+# DB_Name =  "mqtt"
+# hostname = 'localhost'
+# username = 'postgres'
+# pws = 'password@1324'
+# port_id = 5432
 
 # SQLite DB Table Schema
+# TableSchema="""
+# drop table if exists DHT22_Temperature_Data ;
+# drop table if exists DHT22_Humidity_Data ;
+
+# create table Engine_Status (
+#   id serial primary key,
+#   Date_n_Time timestamp,
+#   Engine_Status int
+# );
+
+# create table Working_Hours (
+#   id serial primary key,
+#   Date_n_Time timestamp,
+#   WorkingHours float
+# );
+
+# create table Battery_Level (
+#   id serial primary key,
+#   Date_n_Time timestamp,
+#   BatteryLevel int
+# );
+
+# create table Device_Tampering (
+#   id serial primary key,
+#   Date_n_Time timestamp,
+#   DeviceTampering int
+# );
+
+# create table Engine_RPM (
+#   id serial primary key,
+#   Date_n_Time timestamp,
+#   EngineRPM float
+# );
+
+# create table location (
+#   id serial primary key,
+#   Date_n_Time timestamp,
+#   x float,
+#   y float
+# );
+# """
+
 TableSchema="""
-drop table if exists DHT22_Temperature_Data ;
-drop table if exists DHT22_Humidity_Data ;
-
-create table Engine_Status (
-  id serial primary key,
-  Date_n_Time timestamp,
-  Engine_Status int
-);
-
-create table Working_Hours (
-  id serial primary key,
-  Date_n_Time timestamp,
-  WorkingHours float
-);
-
-create table Battery_Level (
-  id serial primary key,
-  Date_n_Time timestamp,
-  BatteryLevel int
-);
-
-create table Device_Tampering (
-  id serial primary key,
-  Date_n_Time timestamp,
-  DeviceTampering int
-);
-
-create table Engine_RPM (
-  id serial primary key,
-  Date_n_Time timestamp,
-  EngineRPM float
-);
-
-create table location (
-  id serial primary key,
-  Date_n_Time timestamp,
+create table IOT_Data_Spectro_3 (
+  device_IMEI varchar(150),
+  timestamp datetime,
+  firmware_version varchar(150),
+  device_uptime bigint,
+  builtinBattery float,
+  device_tampering int,
+  externalPowerSource int,
+  engine_temp int,
+  RPM int,
+  coolane_level int,
+  fuel_consumption float,
+  fuel_tank_level float,
+  car_speed int,
+  GPS varchar(150),
+  vehicle_distance int,
+  cellular_rssi varchar(150),
+  IMSI varchar(150),
+  MAC varchar(150),
+  total_operation_hour int,
+  car_battery_level varchar(150),
+  fuel_rate varchar(150),
   x float,
   y float
 );
 """
 
 #Connect or Create DB File
-conn = psycopg2.connect(
-        host = hostname,
-        dbname = DB_Name,
-        user = username,
-        password = pws,
-        port = port_id
-  )
+# conn = psycopg2.connect(
+#         host = hostname,
+#         dbname = DB_Name,
+#         user = username,
+#         password = pws,
+#         port = port_id
+#   )
+
+conn= pyodbc.connect('driver={SQL Server}; server=WIN-RD5LI81CPFJ; database=GISDB; UID=sde; PWD=Test@1234;')
 curs = conn.cursor()
 
 #Create Tables
--- mqtt_Publish_Dummy_Data copy.py
+++ mqtt_Publish_Dummy_Data copy.py
@@ -7,29 +7,12 @@ import requests
 # from geoalchemy2.types import Geometry
 from shapely.geometry import Point
 import urllib
-import psycopg2
 # import pandas as pd
 # from sqlalchemy import true
-DB_Name =  "mqtt"
-hostname = 'localhost'
-username = 'postgres'
-pws = 'password@1324'
-port_id = 5432
-curs = None
-conn = None
-#===============================================================
-# Connection initialization
-conn= psycopg2.connect(
-			host = hostname,
-			dbname = DB_Name,
-			user = username,
-			password = pws,
-			port = port_id)
 
-curs = conn.cursor()
 #====================================================
 # MQTT Settings 
-MQTT_Broker = "127.0.0.1"
+MQTT_Broker = "0.0.0.0"
 MQTT_Port = 1883
 Keep_Alive_Interval = 45
 MQTT_Topic_IOT_Data = "IOT/Vehicle/Data"
@@ -73,10 +56,9 @@ def publish_Fake_Sensor_Values_to_MQTT():
 	# global toggle
 	# if toggle == 0:
 	#  - timedelta(hours=2)
-	x = 31.2647639
-	y = 30.0385172
-	location = "31.2647639,30.0385172"
-	id = 0
+	x = 31.4476154
+	y = 30.2211634
+	id = 203
 	while True:
 		id += 1
 		# device_id = int(random.uniform(0, 2))
@@ -96,8 +78,9 @@ def publish_Fake_Sensor_Values_to_MQTT():
 			Battery_Level = 0
 			Device_Tampering = int(random.uniform(0, 2))
 			Engine_RPM = 0
-			x = 31.2647639
-			y = 30.0385172
+			x = 31.343075
+			y = 30.075943
+
 		features=[
 				{
 					"attributes":{
@@ -108,18 +91,18 @@ def publish_Fake_Sensor_Values_to_MQTT():
 					"batterylevel": str(Battery_Level),
 					"devicetampering": str(Device_Tampering),
 					"enginerpm": str(Engine_RPM),
-					"longitude": x,
-					"latitude": y
+					"longitude": str(x),
+					"latitude": str(y)
 					},
 					"geometry" : {
-					"x" : x,
-					"y" : y
+					"x" : str(x),
+					"y" : str(y)
 					}
 				}
 			]
 		print(f"deviceid: {device_id},\n datetime: {Sensor_time},\n enginestatus: {Engine_status},\n workinghours: {Working_Hours},\n batterylevel: {Battery_Level},\n devicetampering: {Device_Tampering},\n enginerpm: {Engine_RPM},\n longitude: {x},\n latitude: {y}")
 
-		url= 'https://192.168.10.17/server/rest/services/IOT_1/FeatureServer/0/addFeatures/'
+		url= 'https://192.168.10.17/server/rest/services/iot/FeatureServer/0/addFeatures/'
 		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 		params = urllib.parse.urlencode({'f': 'json', 'features': json.dumps(features)})
 		r = requests.post(url, verify=False ,data= params, headers= headers)
@@ -134,24 +117,79 @@ def publish_Fake_Sensor_Values_to_MQTT():
 		sensor_data['BatteryLevel'] = Battery_Level
 		sensor_data['DeviceTampering'] = Device_Tampering
 		sensor_data['EngineRPM'] = Engine_RPM
-		sensor_data['longitude'] = str(location.split(",")[0]),
-		sensor_data['latitude'] = str(location.split(",")[1])
+		sensor_data['longitude'] = x
+		sensor_data['latitude'] = y
 		
 		Sensor_json_data = json.dumps(sensor_data)
 
 		print ("Publishing fake IOT Values: ", str(id), str(device_id), str(Sensor_time), str(Engine_status), str(Working_Hours), str(Battery_Level), str(Device_Tampering), str(Engine_RPM), x, y)
-		# publish_To_Topic (MQTT_Topic_IOT_Data, Sensor_json_data)
-		
-		#Push into DB Table
-		insert_script = ("insert into public.iot_data_2 (deviceid, DateTime, EngineStatus, WorkingHours, BatteryLevel, DeviceTampering, EngineRPM, longitude, latitude) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)")
-		insert_value = (device_id, Sensor_time, Engine_status, Working_Hours, Battery_Level, Device_Tampering, Engine_RPM, x, y)
-		curs.execute(insert_script, insert_value)
-		# curs.execute("UPDATE sde.iot_data2 SET shape = ST_SetSRID(ST_MakePoint(longitude, latitude), 4326);")
-		conn.commit()
-		print ("Inserted Fake_IOT_Data into Database.")
-		print ("")
+		publish_To_Topic (MQTT_Topic_IOT_Data, Sensor_json_data)
 		
 		time.sleep(60)
+	# elif toggle ==1:
+	# 	Working_Hours = float("{0:.2f}".format(random.uniform(0, 24)))
+
+	# 	Working_Data = {}
+	# 	Working_Data['Date'] = (datetime.today()).strftime("%d-%b-%Y %H:%M:%S")
+	# 	Working_Data['WorkingHours'] = Working_Hours
+	# 	Working_json_data = json.dumps(Working_Data)
+
+	# 	print ("Publishing fake Working Hours Value: " , str(Working_Hours) , "...")
+	# 	publish_To_Topic (MQTT_Topic_Working_Hours, Working_json_data)
+	# 	toggle = 2
+
+	# elif toggle ==2:
+	# 	Battery_Level = int(random.uniform(25, 100))
+
+	# 	Battery_Data = {}
+	# 	Battery_Data['Date'] = (datetime.today()).strftime("%d-%b-%Y %H:%M:%S")
+	# 	Battery_Data['BatteryLevel'] = Battery_Level
+	# 	Battery_json_data = json.dumps(Battery_Data)
+
+	# 	print ("Publishing fake BatteryLevel Value: " , str(Battery_Level) , "...")
+	# 	publish_To_Topic (MQTT_Topic_Bettery_Level, Battery_json_data)
+	# 	toggle = 3
+
+	# elif toggle ==3:
+	# 	Device_Tampering = int(random.uniform(0, 2))
+
+	# 	Tampering_data = {}
+	# 	Tampering_data['Date'] = (datetime.today()).strftime("%d-%b-%Y %H:%M:%S")
+	# 	Tampering_data['DeviceTampering'] = Device_Tampering
+	# 	Tampering_json_data = json.dumps(Tampering_data)
+
+	# 	print ("Publishing fake Device Tampering Value: " , str(Device_Tampering) , "...")
+	# 	publish_To_Topic (MQTT_Topic_Device_Tampering, Tampering_json_data)
+	# 	toggle = 4
+
+	# elif toggle ==4:
+	# 	Engine_RPM = float("{0:.1f}".format(random.uniform(0, 6)))
+
+	# 	RPM_Data = {}
+	# 	RPM_Data['Date'] = (datetime.today()).strftime("%d-%b-%Y %H:%M:%S")
+	# 	RPM_Data['EngineRPM'] = Engine_RPM
+	# 	RPM_json_data = json.dumps(RPM_Data)
+
+	# 	print ("Publishing fake Engine RPM Value: " , str(Engine_RPM) , "...")
+	# 	publish_To_Topic (MQTT_Topic_Engine_RPM, RPM_json_data)
+	# 	toggle = 5
+
+	# elif toggle ==5:
+	# 	x = float("{0:.1f}".format(random.uniform(0, 6)))
+	# 	y = float("{0:.1f}".format(random.uniform(0, 6)))
+
+	# 	location_data = {}
+	# 	location_data['Date'] = (datetime.today()).strftime("%d-%b-%Y %H:%M:%S")
+	# 	location_data['longitude'] = x
+	# 	location_data['latitude'] = y
+	# 	location_json_data = json.dumps(RPM_Data)
+
+	# 	print ("Publishing fake Engine RPM Value: " , str(x) , str(y) ,  "...")
+	# 	publish_To_Topic (MQTT_Topic_Engine_RPM, location_json_data)
+	# 	toggle = 0
+
+
+		
 
 publish_Fake_Sensor_Values_to_MQTT()
 
